e91d74ec16829658ace06b0e2ba866fd3c9566e9
Tomas Torres <tomasf_torres@mi.unc.edu.ar>
2024-09-26 17:13:41 -0300

    Sem close y cositas

--- kernel/semaphores.c
+++ kernel/semaphores.c
@@ -7,109 +7,138 @@
 
 #define MAX_SEMAPHORES 64
 
-struct semaphore {
-    int value; //valor >= 0
-    struct spinlock lock; //indicador si el semaphore esta bloqueado
-    int init_value;
+struct semaphore
+{
+    int value;            // valor >= 0 o -1 si no esta inicializado
+    struct spinlock lock; // indicador si el semaphore esta bloqueado
+    int init_value;       // siempre >= 0, y 0 si no esta inicializado
 };
 
 // Declaración del arreglo de semáforos
 struct semaphore semaphores[MAX_SEMAPHORES];
 
-
-int sems_pointers[MAX_SEMAPHORES];
+// Variable para el primer sem open
 int initialized = 0;
 
 
-//for (unsigned int i = 0; i < MAX_SEMAPHORES; i++) {
-//   initlock(&semaphores[i].lock, "semaforito");
-//   semaphores[i].value = -1;  // -1 indica no inicializado
-//}
+int sem_open(int sem, int value)
+{
 
-int sem_open(int sem, int value) {
-
-    if (sem >= MAX_SEMAPHORES || sem < 0) {
+    if (sem >= MAX_SEMAPHORES || sem < 0)
+    {
         printf("Error: sem ID invalido\n");
-        return 0;  // Indicando error
+        return 0; // Indicando error
     }
 
     acquire(&(semaphores[sem].lock));
 
-
-    if (!initialized) {
-        for (int i = 0; i < MAX_SEMAPHORES; i++) {
+    if (!initialized)
+    {
+        for (int i = 0; i < MAX_SEMAPHORES; i++)
+        {
             initlock(&semaphores[i].lock, "semaforito");
-            semaphores[i].value = -1;  // -1 indica no inicializado           
+            semaphores[i].value = -1;     // -1 indica no inicializado
+            semaphores[i].init_value = 0; // 0 indica que no tiene valor inicial
         }
         initialized = 1;
     }
-  
-  int res;
-    if (semaphores[sem].value == -1) {
+
+    int res;
+    if (semaphores[sem].value == -1)
+    {
         // solo inicializa si el semáforo no ha sido usado aún
         semaphores[sem].value = value;
         semaphores[sem].init_value = value;
         res = 1;
-    } 
-    else {
+    }
+    else
+    {
         res = 0; // semaforo ya estaba inicializado
     }
 
     release(&(semaphores[sem].lock));
 
     return res;
-  }
+}
 
-int
-sem_close(int sem);
+int sem_close(int sem)
+{
 
-int 
-sem_up (int sem){
+    int value = semaphores[sem].value;
+    int res;
+    acquire(&(semaphores[sem].lock));
+    if (value == -1)
+    {
+        res = 0; // el semaforo no ha sido inicializado
+    }
+    else if (value < 0 || value > semaphores[sem].init_value)
+    {
+        res = 0; // valores invalidos
+    }
+    else
+    {
+        wakeup(&semaphores[sem]);
+        semaphores[sem].value = -1;
+        semaphores[sem].init_value = 0;
+        res = 1;
+    }
+
+    release(&(semaphores[sem].lock));
+
+    return res;
+}
+
+int sem_up(int sem)
+{
     int res;
     int value = semaphores[sem].value;
     acquire(&(semaphores[sem].lock));
-    if(value>semaphores[sem].init_value){
-         res=0;    //ERROR, intenta habilitar más procesos que los inicializados por el semaforo
+    if (value >= semaphores[sem].init_value)
+    {
+        res = 0; // ERROR, intenta habilitar más procesos que los inicializados por el semaforo
     }
-    else if(value>0){
+    else if (value > 0)
+    {
         semaphores[sem].value++;
-        res=1;
+        res = 1;
     }
-    else if (value==-1){
-           res=0;         // Error semaforo no inicializado
+    else if (value == -1)
+    {
+        res = 0; // Error semaforo no inicializado
     }
-    else if(value==0){
+    else if (value == 0)
+    {
         semaphores[sem].value++;
-    wakeup(&semaphores[sem]);  //Se rehabilita el semaforo
-    res=1;
+        wakeup(&semaphores[sem]); // Se rehabilita el semaforo
+        res = 1;
     }
-    else{
-      res=0;  // ERROR Value no esperado 
-       }
-release(&(semaphores[sem].lock));
-return res;
-
+    else
+    {
+        res = 0; // ERROR Value no esperado
+    }
+    release(&(semaphores[sem].lock));
+    return res;
 }
 
-
-int
-sem_down(int sem){
-    int value  = semaphores[sem].value;
+int sem_down(int sem)
+{
+    int value = semaphores[sem].value;
     acquire(&(semaphores[sem].lock));
-    if(value>0){
+    if (value > 0)
+    {
         semaphores[sem].value--;
-
-    }else if(value < 0){
-       
-     release(&(semaphores[sem].lock)); 
-     
-        return 0;      // Semaforo no inicializado
-
-    }  
-    while (value == 0) {
-        sleep(&semaphores[sem], &(semaphores[sem].lock)); 
     }
-  
+    else if (value < 0)
+    {
+
+        release(&(semaphores[sem].lock));
+
+        return 0; // Semaforo no inicializado
+    }
+    while (value == 0)
+    {
+        sleep(&semaphores[sem], &(semaphores[sem].lock));
+    }
 
     release(&(semaphores[sem].lock));
     return 1;
