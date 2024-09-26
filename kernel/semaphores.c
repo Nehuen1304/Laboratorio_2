0d48af84a8bdc6312c5a9e4a875e8100c24ea6e1
Nehuen1304 <nehuen.guevara@mi.unc.edu.ar>
2024-09-26 16:27:01 -0300

    Sem_up y algunas modificaciones

--- kernel/semaphores.c
+++ kernel/semaphores.c
@@ -18,6 +18,8 @@ struct semaphore semaphores[MAX_SEMAPHORES];
 
 
 int sems_pointers[MAX_SEMAPHORES];
+int initialized = 0;
+
 
 //for (unsigned int i = 0; i < MAX_SEMAPHORES; i++) {
 //   initlock(&semaphores[i].lock, "semaforito");
@@ -28,17 +30,16 @@ int sem_open(int sem, int value) {
 
     if (sem >= MAX_SEMAPHORES || sem < 0) {
         printf("Error: sem ID invalido\n");
-        return -1;  // Indicando error
+        return 0;  // Indicando error
     }
 
     acquire(&(semaphores[sem].lock));
 
 
-  static int initialized = 0;
     if (!initialized) {
         for (int i = 0; i < MAX_SEMAPHORES; i++) {
             initlock(&semaphores[i].lock, "semaforito");
-            semaphores[i].value = -1;  // -1 indica no inicializado
+            semaphores[i].value = -1;  // -1 indica no inicializado           
         }
         initialized = 1;
     }
@@ -62,8 +63,34 @@ int sem_open(int sem, int value) {
 int
 sem_close(int sem);
 
-int
-sem_up(int sem);
+int 
+sem_up (int sem){
+    int res;
+    int value = semaphores[sem].value;
+    acquire(&(semaphores[sem].lock));
+    if(value>semaphores[sem].init_value){
+         res=0;    //ERROR, intenta habilitar más procesos que los inicializados por el semaforo
+    }
+    else if(value>0){
+        semaphores[sem].value++;
+        res=1;
+    }
+    else if (value==-1){
+           res=0;         // Error semaforo no inicializado
+    }
+    else if(value==0){
+        semaphores[sem].value++;
+    wakeup(&semaphores[sem]);  //Se rehabilita el semaforo
+    res=1;
+    }
+    else{
+      res=0;  // ERROR Value no esperado 
+       }
+release(&(semaphores[sem].lock));
+return res;
+
+}
+
 
 int
 sem_down(int sem){
@@ -72,15 +99,15 @@ sem_down(int sem){
     if(value>0){
         semaphores[sem].value--;
 
-
     }else if(value < 0){
-        printf("Semaforo no inicializado");
-        release(&(semaphores[sem].lock));
-        return 0;
+       
+     release(&(semaphores[sem].lock)); 
+     
+        return 0;      // Semaforo no inicializado
 
     }  
     while (value == 0) {
-        sleep(&semaphores[sem], &(semaphores[sem].lock));
+        sleep(&semaphores[sem], &(semaphores[sem].lock)); 
     }
   
 
